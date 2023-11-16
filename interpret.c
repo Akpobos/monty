9b827605923981f107f5ffcb45f30706ba4d2882
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-11-16 01:40:55 -0800

    dd

--- interpret.c
+++ interpret.c
@@ -14,14 +14,10 @@ void interpret(stack_t **stack, char *line, unsigned int ln)
 	unsigned int i;
 	void (*f)(stack_t **stack, unsigned int ln) = NULL;
 	instruction_t ops[] = {
-		{ "push", push },
-		{ "pint", pint },
-		{ "pall", pall },
-		{ "pop", pop },
-		{ "nop", nop },
-		{ "swap", swap },
-		{ "add", add },
-		{ "sub", sub },
+		{ "push", push }, { "pint", pint }, { "pall", pall },
+		{ "pop", pop }, { "nop", nop }, { "swap", swap },
+		{ "add", add }, { "sub", sub }, {"div", div},
+		{ "mul", mul },
 		{ NULL, NULL }
 	};
 
--- op_2.c
+++ op_2.c
@@ -71,3 +71,51 @@ void sub(stack_t **stack, unsigned int line_number)
 	*stack = tmp->next;
 	free(tmp);
 }
+
+/**
+ * div - divide top two
+ * @stack: the list
+ * @line_number: Current line in monty file
+ * Return: Nothing
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+	int result;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		free_stack(stack);
+		hndlerr("can't div, stack too short", NULL, line_number);
+	}
+	result = (*stack)->next->n / (*stack)->n;
+	(*stack)->next->n = result;
+	(*stack)->next->prev = NULL;
+	tmp = (*stack);
+	*stack = tmp->next;
+	free(tmp);
+}
+
+/**
+ * mul - multiply top two
+ * @stack: the list
+ * @line_number: Current line in monty file
+ * Return: Nothing
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+	int result;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		free_stack(stack);
+		hndlerr("can't mul, stack too short", NULL, line_number);
+	}
+	result = (*stack)->next->n * (*stack)->n;
+	(*stack)->next->n = result;
+	(*stack)->next->prev = NULL;
+	tmp = (*stack);
+	*stack = tmp->next;
+	free(tmp);
+}
